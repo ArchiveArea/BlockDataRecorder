6771b9e12932b8a1d4209edef585c418e32a3751
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2023-04-24 19:34:41 +0700

    BlockDataRecorder

---
+++ README.md
@@ -0,0 +1,6 @@
+# General
+**The data(CustomName, Lore, Enchants,...) of an item block will not be lost when the block is placed**
+
+# Contact
+[![Discord](https://img.shields.io/discord/986553214889517088?label=discord&color=7289DA&logo=discord)](https://discord.gg/j2X83ujT6c)\
+**You can contact me directly via Discord `NhanAZ#9115`**
---
+++ src/NhanAZ/BlockDataRecorder/Main.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NhanAZ\BlockDataRecorder;
+
+use NhanAZ\BlockData\BlockData;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\Listener;
+use pocketmine\item\Item;
+use pocketmine\item\ItemBlock;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener {
+
+	protected BlockData $blockData;
+
+	protected function onEnable(): void {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->blockData = new BlockData($this, false);
+	}
+
+	private function jsonStringToItem(string $string): Item {
+		$itemDecoded = json_decode($string, true);
+		$itemDeserialize = Item::jsonDeserialize($itemDecoded);
+		return $itemDeserialize;
+	}
+
+	public function onBlockPlace(BlockPlaceEvent $event): void {
+		$item = $event->getItem();
+		$block = $event->getBlock();
+		if ($item instanceof ItemBlock) {
+			$itemSerialize = $item->jsonSerialize($item);
+			$itemEncoded = json_encode($itemSerialize);
+			$this->blockData->setData($block, $itemEncoded);
+		}
+	}
+
+	public function onBlockBreak(BlockBreakEvent $event): void {
+		if (!$event->isCancelled()) {
+			$block = $event->getBlock();
+			$blockData = $this->blockData->getData($block);
+			if ($blockData !== null) {
+				$itemDrop = $this->jsonStringToItem($blockData);
+				if ($itemDrop instanceof ItemBlock) {
+					$itemDrop->setCount(1);
+					$event->setDrops([$itemDrop]);
+				}
+			}
+		}
+	}
+}
